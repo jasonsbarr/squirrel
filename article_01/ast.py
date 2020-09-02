75fbb196e8bfcd2f5aba0dba29fc6ad5f1320490
Jason Barr <me@jasonbarr.dev>
2020-09-02 16:22:30 -0500

    Add base AST, Program, and NumericLiteral AST nodes

---
+++ article_01/ast.py
@@ -0,0 +1,25 @@
+class ASTNode:
+
+    def __init__(self, node_type, start, end):
+        self.type = node_type
+        self.start = start
+        self.end = end
+
+
+class ProgramNode(ASTNode):
+
+    def __init__(self, tokens):
+        self.type = "Program"
+        self.start = {"line": tokens[0].line, "col": tokens[0].start}
+        self.end = {"line": tokens[-1].line, "col": tokens[-1].end}
+        self.children = []
+
+
+class NumberNode(ASTNode):
+
+    def __init__(self, value, start, end):
+        super().__init__("NumericLiteral", start, end)
+        self.value = value
+
+    def __repr__(self):
+        return f"NumberNode({self.value}, {self.start}, {self.end})"
