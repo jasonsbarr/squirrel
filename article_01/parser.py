50125776073b5c53c6084b43d9c3c09c991d2b58
Jason Barr <me@jasonbarr.dev>
2020-09-02 18:32:20 -0500

    Refactor start and end points as Point class

--- article_01/parser.py
+++ article_01/parser.py
@@ -1,6 +1,19 @@
 from ast import ProgramNode, NumberNode
 
 
+class Point:
+
+    def __init__(self, line, col):
+        self.line = line
+        self.col = col
+
+    def __str__(self):
+        return f"({self.line}:{self.col})"
+
+    def __repr__(self):
+        return f"Point({self.line}, {self.col})"
+
+
 def parse(tokens):
     ast = ProgramNode(tokens)
     pos = 0
@@ -13,10 +26,11 @@ def parse(tokens):
 
     while not token.type == "EOF":
         if token.type == "NUMBER":
-            ast.children.append(NumberNode(
-                token.value, {
-                    "line": token.line, "col": token.start}, {
-                    "line": token.line, "col": token.end}))
+            ast.children.append(
+                NumberNode(
+                    token.value, Point(
+                        token.line, token.col), Point(
+                        token.line, token.col)))
 
         token = get_next_token()
 
