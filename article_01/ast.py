14716f8dea0cb71ff791527a358db1b5634220e2
Jason Barr <me@jasonbarr.dev>
2020-09-02 16:33:19 -0500

    Clarify string representation of AST nodes, correctly parse NumericLiteral tokens

--- article_01/ast.py
+++ article_01/ast.py
@@ -10,9 +10,16 @@ class ProgramNode(ASTNode):
 
     def __init__(self, tokens):
         self.type = "Program"
+        self.tokens = tokens
         self.start = {"line": tokens[0].line, "col": tokens[0].start}
         self.end = {"line": tokens[-1].line, "col": tokens[-1].end}
         self.children = []
+
+    def __repr__(self):
+        return f"ProgramNode({self.tokens})"
+
+    def __str__(self):
+        return f"{self.type}, children: {self.children}"
 
 
 class NumberNode(ASTNode):
--- article_01/parser.py
+++ article_01/parser.py
@@ -1,10 +1,10 @@
-from article_01.ast import ProgramNode, NumberNode
+from ast import ProgramNode, NumberNode
 
 
 def parse(tokens):
     ast = ProgramNode(tokens)
-    pos = -1
-    token = None
+    pos = 0
+    token = tokens[pos]
 
     def get_next_token():
         nonlocal pos
@@ -12,10 +12,12 @@ def parse(tokens):
         return tokens[pos]
 
     while not token.type == "EOF":
-        token = get_next_token()
-
         if token.type == "NUMBER":
             ast.children.append(NumberNode(
                 token.value, {
                     "line": token.line, "col": token.start}, {
                     "line": token.line, "col": token.end}))
+
+        token = get_next_token()
+
+    return ast
