d1ec4c2dc1e9527b17d6c6cb885dc0ace274558f
Jason Barr <me@jasonbarr.dev>
2020-09-02 16:24:42 -0500

    Make token variable name consistent

--- article_01/parser.py
+++ article_01/parser.py
@@ -4,13 +4,14 @@ from article_01.ast import ProgramNode, NumberNode
 def parse(tokens):
     ast = ProgramNode(tokens)
     pos = -1
+    token = None
 
     def get_next_token():
         nonlocal pos
         pos += 1
         return tokens[pos]
 
-    while not current.type == "EOF":
+    while not token.type == "EOF":
         token = get_next_token()
 
         if token.type == "NUMBER":
