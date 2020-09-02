f9543a2abcf618c14ed48bcfb4e42bbfeabd0f98
Jason Barr <me@jasonbarr.dev>
2020-09-02 17:05:18 -0500

    Evaluate numeric literal expressions

--- article_01/interpreter.py
+++ article_01/interpreter.py
@@ -1,2 +1,20 @@
 def evaluate(ast):
-    return ast
+    pos = 0
+    child = ast.children[pos]
+
+    def get_next_child():
+        nonlocal pos
+        pos += 1
+        return ast.children[pos]
+
+    def eval_expr(node):
+        if node.type == "NumericLiteral":
+            return node.value
+
+    while pos < len(ast.children):
+        current_value = eval_expr(child)
+
+        try:
+            child = get_next_child()
+        except IndexError:
+            return current_value
