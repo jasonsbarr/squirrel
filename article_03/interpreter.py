3330373865cbcf2b34ad5eed4bc1097bae360890
Jason Barr <me@jasonbarr.dev>
2020-09-03 12:53:39 -0500

    Evaluate binary arithmetic expressions

--- article_03/interpreter.py
+++ article_03/interpreter.py
@@ -8,8 +8,25 @@ def evaluate(ast):
         return ast.children[pos]
 
     def eval_expr(node):
+        if node.type == "BinaryOperation":
+            return apply_op(
+                node.op, eval_expr(
+                    node.left), eval_expr(
+                    node.right))
         if node.type == "NumericLiteral":
             return node.value
+
+    def apply_op(op, left, right):
+        if op.value == "+":
+            return left + right
+        if op.value == "-":
+            return left - right
+        if op.value == "*":
+            return left * right
+        if op.value == "/":
+            return left / right
+        if op.value == "%":
+            return left % right
 
     while pos < len(ast.children):
         current_value = eval_expr(child)
