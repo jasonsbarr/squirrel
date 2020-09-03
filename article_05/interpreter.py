317a3d1f647d02e2cc69b1ad61334e470670f997
Jason Barr <me@jasonbarr.dev>
2020-09-03 15:48:57 -0500

    Copy article_04 code to article_05

---
+++ article_05/interpreter.py
@@ -0,0 +1,43 @@
+def evaluate(ast):
+    pos = 0
+    child = ast.children[pos]
+
+    def get_next_node():
+        nonlocal pos
+        pos += 1
+        return ast.children[pos]
+
+    def eval_expr(node):
+        if node.type == "BinaryOperation":
+            return apply_op(
+                node.op, eval_expr(
+                    node.left), eval_expr(
+                    node.right))
+
+        if node.type == "UnaryOperation":
+            return apply_unary(node.op, eval_expr(node.expr))
+
+        if node.type == "NumericLiteral":
+            return node.value
+
+    def apply_op(op, left, right):
+        if op.value == "+":
+            return left + right
+        if op.value == "-":
+            return left - right
+        if op.value == "*":
+            return left * right
+        if op.value == "/":
+            return left / right
+
+    def apply_unary(op, expr):
+        if op.value == "-":
+            return -expr
+
+    while pos < len(ast.children):
+        current_value = eval_expr(child)
+
+        try:
+            child = get_next_node()
+        except IndexError:
+            return current_value
