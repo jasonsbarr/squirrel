d9bd494d7d2e9e16998c8466aa8ae0d6013ee8a3
Jason Barr <me@jasonbarr.dev>
2020-09-04 11:36:56 -0500

    Create basic standard library

---
+++ part_04/stdlib.py
@@ -0,0 +1,30 @@
+def sum_all(*args):
+    return sum(args)
+
+
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def mul(x, y):
+    return x * y
+
+
+def div(x, y):
+    return x / y
+
+
+def get_pow(base, exp):
+    return base ** exp
+
+
+def get_max(*args):
+    return max(args)
+
+
+def get_min(*args):
+    return min(args)
