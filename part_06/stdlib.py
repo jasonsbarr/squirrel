3ebb50874245023a21a450839da565091d1cdb0b
Jason Barr <me@jasonbarr.dev>
2020-09-05 16:40:50 -0500

    Copy part_05 files to part_06

---
+++ part_06/stdlib.py
@@ -0,0 +1,25 @@
+def sum(*args):
+    s = 0
+    for arg in args:
+        s += arg
+    return s
+
+
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def mul(x, y):
+    return x * y
+
+
+def div(x, y):
+    return x / y
+
+
+def pow(base, exp):
+    return base ** exp
